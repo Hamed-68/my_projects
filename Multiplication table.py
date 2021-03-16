cfd54e195e419c9e870e75de154a77380132ba45
Hamed-68 <hamedbakhti918@yahoo.com>
2021-03-16 11:31:28 +0330

    change variable "a" and "b" to "first_number" and "second_number"

--- Multiplication table.py
+++ Multiplication table.py
@@ -7,25 +7,25 @@ right_answer = 0  ##all of your right answer
 wrong_answer = 0  ##all of your wrong answer
 
 for i in range(10):
-    a = randint(1, 10)
-    b = randint(1, 10)
+    first_number = randint(1, 10)
+    second_number = randint(1, 10)
 
     t1 = time.perf_counter() ##START TIME
     print(f"{a} * {b}")
     answer = int(input())
     t2 = time.perf_counter() ##END TIME
 
-    if (t2 - t1) <= 3 and answer == (a * b): ## 3 SECOND TIME TO TYPE RIGHT ANSWER
+    if (t2 - t1) <= 3 and answer == (first_number * second_number): ## 3 SECOND TIME TO TYPE RIGHT ANSWER
         print("right answer!!!")
         right_answer += 1
-    elif (t2 - t1) <= 3 and answer != a * b:
+    elif (t2 - t1) <= 3 and answer != first_number * second_number:
         print("wrong answer!!!")
         wrong_answer += 1
-    elif (t2 - t1) > 3 and answer == a * b:
+    elif (t2 - t1) > 3 and answer == first_number * second_number:
         print("you lose time!!!")
         wrong_answer += 1
         print(t2 - t1)
-    elif (t2 - t1) > 3 and answer != a * b:
+    elif (t2 - t1) > 3 and answer != first_number * second_number:
         print("wrong answer!! lose time!!")
         wrong_answer += 1
         print(t2 - t1)
